34c0a6b771ef86709a78bcbd874d86f55684f885
luis24552-cmd <luis24552@cbtis75.edu.mx>
2026-04-28 20:00:12 -0600

    actualización del proyecto

--- server.js
+++ server.js
@@ -33,8 +33,9 @@ app.get("/usuarios", async (req, res) => {
 });
 
 // 🚀 SERVIDOR
-app.listen(3000, () => {
-    console.log("🔥 Servidor corriendo en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🔥 Servidor corriendo en el puerto ${PORT}`);
 });
 
 app.post("/usuarios", async (req, res) => {
